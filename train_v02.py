6496c81cf94db90c50f88451859fae20f6549b59
nikandeveloper <93449288+nikandeveloper@users.noreply.github.com>
2026-08-24 12:42:48 +0200

    Update train_v02.py

--- train_v02.py
+++ train_v02.py
@@ -1,37 +1,42 @@
 import torch
 import torch.nn as nn
-import model_v02 as model
+import model
+import tokeniser as t
+
+t.ELEMENTS = [
+    element
+    for element in t.ELEMENTS
+    if element not in t.ALIPHATIC
+]
 
 lines = []
 
 inp_side = []
 out_side = []
 
-vocab = {}
+vocab_size = 0
+
 
 with open("reaction_database.txt", "r") as file:
   for line in file:
-    print(line)
     i, o = line.rstrip("\n").split(">>")[0], line.rstrip("\n").split(">>")[1]
     mi = []
     mo = []
 
-    for k in i:
-      if k not in vocab:
-          vocab[k] = len(vocab)
-      mi.append(vocab[k])   
-    for k in o:
-      if k not in vocab:
-          vocab[k] = len(vocab)
-      mo.append(vocab[k])   
- 
+    readeri = t.SmilesReader(i)
+    readero = t.SmilesReader(o)
+
+    mi = readeri.tokenise()
+    mo = readero.tokenise()
+    
+    vocab_size = readeri.vocab_size()
+
     inp_side.append(torch.tensor(mi))
     out_side.append(torch.tensor(mo))
  
 
-vol = len(vocab)
 
-model = model.Seq2Seq(vol+2, 256, 4, 128, vol, vol+1)
+model = model.Seq2Seq(vocab_size+2, 256, 4, 128, vocab_size, vocab_size+1)
 
 criteron = nn.CrossEntropyLoss()
 
@@ -40,7 +45,7 @@ optimiser = torch.optim.Adam(model.parameters(), lr=0.0001)
 for epoch in range(10):
  for u in range(len(inp_side)):
    src = inp_side[u]
-   trg = torch.cat([torch.tensor([vol]),out_side[u], torch.tensor([vol+1])])
+   trg = torch.cat([torch.tensor([vocab_size]),out_side[u], torch.tensor([vocab_size+1])])
    optimiser.zero_grad()
 
    logits = model.train_step(src, trg)
@@ -64,3 +69,6 @@ with torch.no_grad():
 
 print("Predicted tokens")
 print(predicted_tokens)
+
+
+torch.save(model.state_dict(), "model_v02.pth")
