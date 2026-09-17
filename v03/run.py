3a556a244a2c25ef738bfcde35348d2c1ceb6a02
nikandeveloper <93449288+nikandeveloper@users.noreply.github.com>
2026-09-17 14:14:13 +0200

    added padding

--- v03/run.py
+++ v03/run.py
@@ -11,11 +11,12 @@ from pathlib import Path
 
 
 
-CSV_RAW_REACTIONS_FILE_NAME = "raw_test.csv"
+CSV_RAW_REACTIONS_FILE_NAME = "raw_train.csv"
 PICKELED_REACTION_DATABASE_FILE_NAME = "reactions_test.db"
 PTH_MODEL_NAME = "model.pth"
 
-
+penalty_importance = 1
+ignoring_index = -100
 
 
 reactions_file = Path(__file__).parent / PICKELED_REACTION_DATABASE_FILE_NAME
@@ -23,7 +24,7 @@ model_file = Path(__file__).parent / PTH_MODEL_NAME
 
 if reactions_file.is_file():
 
-  with open(PICKELED_REACTION_DATABASE_FILE_NAME, "rb") as file:
+  with open(reactions_file, "rb") as file:
 
     database = pickle.load(file)
 
@@ -41,13 +42,15 @@ else:
 
 
 vocab_size = t.SmilesReader.vocab_size()
+print(vocab_size)
 
 model = md.Seq2Seq(vocab_size+2, 256, 3, 256, vocab_size, vocab_size+1)
 
 if model_file.is_file():
   try:
-    state_dict = torch.load("model.pth", weights_only=True)
-    model.load_state_dict(state_dict)
+    state_dict = torch.load("model.pth", weights_only=True, map_location=torch.device("cpu"))
+    model.load_state_dict(state_dict["model_state_dict"])
+    print(state_dict["loss"])
 
   except:
     raise ValueError(f"The state dict in file {PTH_MODEL_NAME} is corrupted")
@@ -55,7 +58,7 @@ if model_file.is_file():
 else:
   raise ValueError(f"This file does not exist: {PTH_MODEL_NAME}")
 
-criteron = nn.CrossEntropyLoss(ignore_index=-100)
+criteron = nn.CrossEntropyLoss(ignore_index=ignoring_index)
 
 examine_loss = 0
 
@@ -63,17 +66,22 @@ for u in range(len(database.reactions)):
   src = database.reactions[u].reactants_tokens
   trg = torch.cat([torch.tensor([vocab_size]),database.reactions[u].products_tokens, torch.tensor([vocab_size+1])])
 
-  logits = model.forward(src)
+  logits = model.train_step(src, trg)
+  
+  d = logits.shape[0] - trg[1:].shape[0]
 
-  if logits.shape[0] < trg[1:].shape[0]:
-    pad = torch.full((trg[1:].shape[0] - logits.shape[0],), -100, dtype=trg.dtype, device=trg.device)
-    new_target = torch.cat([trg[1:], pad])
-  else:
-    new_target = trg[1:logits.shape[0]]
+  factor = d > 0
+  pad = torch.full((logits.shape[0] - trg[1:].shape[0],), -100, dtype=trg.dtype, device=trg.device)
+  padded = torch.cat([trg[1:], pad])
+  cut = trg[1:logits.shape[0] + 1]
+  new_target = torch.where(factor, padded, cut)
 
   loss = criteron(logits, new_target)
 
+  loss = loss + (abs(d)+d)/(2*vocab_size)
+
   examine_loss += loss.item()
+  print(loss.item())
 
 
 
