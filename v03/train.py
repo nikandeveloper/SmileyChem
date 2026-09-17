d83794a009836969e437a4187e11e030b024b70e
nikandeveloper <93449288+nikandeveloper@users.noreply.github.com>
2026-09-17 14:15:24 +0200

    added bucket batching
    added bucket batching and made the training loop more TPU friendly

--- v03/train.py
+++ v03/train.py
@@ -11,9 +11,10 @@ from pathlib import Path
 
 
 
-CSV_RAW_REACTIONS_FILE_NAME = "raw_test.csv"
+CSV_RAW_REACTIONS_FILE_NAME = "raw_train.csv"
 PICKELED_REACTION_DATABASE_FILE_NAME = "reactions.db"
 PTH_MODEL_NAME = "model.pth"
+
 
 CHECKPOINT_DIR = Path("checkpoint")
 CHECKPOINT_DIR.mkdir(exist_ok=True)
@@ -22,10 +23,10 @@ CHECKPOINT_LENGTH = 10 * 60 #assuming time in seconds
 
 
 starting_epoch = 0
-starting_reaction = 0
+starting_batch = 0
 EPOCHS = 100
 
-
+ignoring_index = -100
 
 
 
@@ -35,12 +36,17 @@ if torch.cuda.is_available():
   print(torch.cuda.get_device_name(0))
 
 
+
 reactions_file = Path(__file__).parent / PICKELED_REACTION_DATABASE_FILE_NAME
 model_file = Path(__file__).parent / PTH_MODEL_NAME
 
+vocab_size = t.SmilesReader.vocab_size()
+
+
+
 if reactions_file.is_file():
 
-  with open(PICKELED_REACTION_DATABASE_FILE_NAME, "rb") as file:
+  with open(reactions_file, "rb") as file:
 
     database = pickle.load(file)
 
@@ -50,6 +56,8 @@ else:
   database.load_data()
   database.canonicalise_mapped()
   database.tokenise()
+  database.add_marking_codes(vocab_size + 1, vocab_size)
+  database.bucket_batch(-100)
 
   with open(PICKELED_REACTION_DATABASE_FILE_NAME, "wb") as file:
    
@@ -57,15 +65,14 @@ else:
 
 
 
-vocab_size = t.SmilesReader.vocab_size()
 
 
 model = md.Seq2Seq(vocab_size+2, 256, 3, 256, vocab_size, vocab_size+1)
 
 if model_file.is_file():
   try:
-    state_dict = torch.load("model.pth", weights_only=True)
-    model.load_state_dict(state_dict)
+    state_dict = torch.load(model_file, weights_only=True)
+    model.load_state_dict(state_dict["model_state_dict"])
 
   except:
     raise ValueError(f"The state dict in file {PTH_MODEL_NAME} is corrupted")
@@ -73,23 +80,29 @@ if model_file.is_file():
 else:
   print(f"File {PTH_MODEL_NAME} does not existing. Making new model...")
 
-model.to(device)
+
+model = model.to(device)
 
 
-criteron = nn.CrossEntropyLoss()
+criteron = nn.CrossEntropyLoss(ignore_index=ignoring_index)
 
 optimiser = torch.optim.Adam(model.parameters(), lr=0.0001)
 
 model.train()
 
+
 last_checkpoint = time.time()
 
 
-for epoch in range(starting_epoch, EPOCHS):
+for epoch in range(EPOCHS):
+  
   examine_loss = 0
-  for u in range(starting_reaction, len(database.reactions)):
-   src = database.reactions[u].reactants_tokens
-   trg = torch.cat([torch.tensor([vocab_size]),database.reactions[u].products_tokens, torch.tensor([vocab_size+1])])
+  
+  for key in database.bucket_dict_reactant.keys():
+   
+   src = database.bucket_dict_reactant[key]
+   trg = database.bucket_dict_product[key]
+   
    optimiser.zero_grad()
 
    src = src.to(device)
@@ -99,7 +112,7 @@ for epoch in range(starting_epoch, EPOCHS):
 
    loss = criteron(logits, trg[1:])
 
-   examine_loss = loss.item()
+   examine_loss += loss.item()
 
    loss.backward()
 
@@ -108,8 +121,8 @@ for epoch in range(starting_epoch, EPOCHS):
    optimiser.step()
 
    if time.time() - last_checkpoint >= CHECKPOINT_LENGTH:
-     check_point_file = CHECKPOINT_DIR / f"model_epoch_{epoch}_reaction_{u}.pth"
-     saved_data = {"epoch": epoch, "reaction": u, "model_state_dict": model.state_dict(),  "optimiser_state_dict": optimiser.state_dict(), "loss": loss.item()}
+     check_point_file = CHECKPOINT_DIR / f"model_epoch_{epoch}_batch_key_{key}.pth"
+     saved_data = {"epoch": epoch, "batch_key": key, "model_state_dict": model.state_dict(),  "optimiser_state_dict": optimiser.state_dict(), "loss": loss.item()}
 
      torch.save(saved_data, check_point_file)
 
@@ -119,6 +132,10 @@ for epoch in range(starting_epoch, EPOCHS):
      last_checkpoint = time.time()
 
 
+  print(epoch, examine_loss/len(database.bucket_dict_reactant))
 
 
-torch.save(model.state_dict(), "model.pth")
+
+
+saved_data = {"epoch": EPOCHS, "model_state_dict": model.state_dict(),  "optimiser_state_dict": optimiser.state_dict(), "loss": examine_loss}
+torch.save(saved_data, model_file)
