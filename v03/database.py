c4296ff2f1edec4914ccea1fd0196537d91df4bb
nikandeveloper <93449288+nikandeveloper@users.noreply.github.com>
2026-09-17 14:12:11 +0200

    added bucket batching
    added bucket batching

--- v03/database.py
+++ v03/database.py
@@ -3,6 +3,7 @@ import validation
 from canonicalisation import canonicalise_reaction_mapped, canonicalise_reaction_unmapped
 from parser import Reaction
 from dataclasses import dataclass, field
+import torch
 
 ignore = "id,class,reactants>reagents>production\n"
 
@@ -10,10 +11,18 @@ ignore = "id,class,reactants>reagents>production\n"
 class Database:
   file_name: str
   ignore_line: str | None = None
+  size_bucket: int
+
 
   reactions: list[Reaction] = field(default_factory=list)
 
+  bucket_dict_reactant: dict[int ,torch.Tensor] = field(default_factory=dict)
+  bucket_dict_product: dict[int ,torch.Tensor] = field(default_factory=dict)
+
+  len_bucket_dict: dict[int ,list[int]] = field(default_factory=dict)
+
   mapping_nums: list[tuple[set[int], set[int], set[int]]] = field(default_factory=list)
+
 
   def load_data(self):
     with open(self.file_name, "r") as file:
@@ -31,6 +40,40 @@ class Database:
         reaction = validation.validate_reaction(reaction)
 
         self.reactions.append(reaction)
+
+  def add_marking_codes(self, EOS, SOS):
+    for key in database.bucket_dict_reactant.keys():
+        database.bucket_dict_product[key] = torch.cat([
+          src.new_full((src.size(0), 1), SOS),
+          database.bucket_dict_product[key],    
+          src.new_full((src.size(0), 1), EOS)
+        ], dim=1)
+   
+
+  def bucket_batch(self, padding: int):
+    for reaction in self.reactions:
+      key = self.index_bucket(reaction.max_len)
+      reaction.key = key
+      self.len_bucket_dict.setdefault(key, []).append(reaction.max_len)
+
+    for reaction in self.reactions:
+      reaction.padded_tokens = reaction.pad(padding, max(self.len_bucket_dict[reaction.key]))
+
+      self.bucket_dict_reactant[reaction.key] = (reaction.padded_tokens[0].unsqueeze(0) if self.bucket_dict_reactant.get(reaction.key) is None else torch.cat([self.bucket_dict_reactant[reaction.key], reaction.padded_tokens[0].unsqueeze(0)], dim=0))
+      self.bucket_dict_product[reaction.key] = (reaction.padded_tokens[1].unsqueeze(0) if self.bucket_dict_product.get(reaction.key) is None else torch.cat([self.bucket_dict_product[reaction.key], reaction.padded_tokens[1].unsqueeze(0)], dim=0))
+
+
+
+  def index_bucket(self, max_len: int) -> int:
+    for bucket in self.len_bucket_dict:
+      if abs(bucket - max_len) < self.size_bucket / 2:
+        return bucket
+
+    bucket = (max_len // self.size_bucket) * self.size_bucket
+    if max_len % self.size_bucket >= self.size_bucket / 2:
+      bucket += self.size_bucket
+
+    return bucket  
 
 
   def canonicalise_mapped(self):
